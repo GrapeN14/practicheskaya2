f9144fcb9ccdeab2047566be2cbd91becdff5199
ADMIN <ADMIN@DESKTOP-HRD3VOD>
2024-02-02 00:39:54 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите номер месяца: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            switch (x)
+            {
+                case 1: Console.WriteLine("Зима \nДекабрь - 31, Январь - 31, Февраль - 29"); break;
+                case 2: Console.WriteLine("Весна \nМарт - 31, Апрель - 30, Май - 31"); break;
+                case 3: Console.WriteLine("Лето \nИюнь - 30, Июль - 31, Август - 31"); break;
+                case 4: Console.WriteLine("Осень \nСентябрь - 30, Октябрь - 31, Ноябрь - 31"); break;
+                default: Console.WriteLine("Нет такого времени года"); break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите четыре целых числа: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            int d = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\n");
+            {
+                if (a % 2 == 0)
+                {
+                    Console.WriteLine(a);
+                }
+                if (b % 2 == 0)
+                {
+                    Console.WriteLine(b);
+                }
+                if (c % 2 == 0)
+                {
+                    Console.WriteLine(c);
+                }
+                if (d % 2 == 0)
+                {
+                    Console.WriteLine(d);
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
